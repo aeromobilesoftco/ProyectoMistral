dcb2f3d24a1095b92f66ca12ce92448eba2ad9cd
AeroMobileSoft Colombia <gerencia@aeromobilesoft.com.co>
2016-09-16 22:36:17 -0500

    Cambios viernes 16/09/2016
    Se crea el modulo de simmulacion de ganancia. queda en desarrollo el
    comando ng-required

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('app.controllers', [])
+angular.module('app.controllers', ['ngCordova'])
   
 .controller('inicioMistralEconoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
@@ -56,11 +56,20 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('simuladorDeGananciasCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('simuladorDeGananciasCtrl', ['$scope', '$stateParams','$cordovaDialogs',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams,$cordovaDialogs) {
 
+$scope.simlimpia={};
+
+$scope.clksimlim=function(){
+
+	$scope.simgan = angular.copy($scope.simlimpia);
+
+}
+// Se llama a la funcion para que se ejecute como una recarga de pagina
+$scope.clksimlim();
 
 }])
    
