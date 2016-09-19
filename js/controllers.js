213c9307c4efdcab8adf31b711ce792fbea8c960
AeroMobileSoft Colombia <gerencia@aeromobilesoft.com.co>
2016-09-19 17:51:37 -0500

    Modulo de soporte y ayuda
    Se deja hecha la estructura del envio de correo. Solo falta obtener los
    datos de la vista del usuario. ATENCION: NO OLVIDE INSTALAR EL
    COMPLEMENTE EMAIL COMPOSER DE NGCORDOVA.

--- js/controllers.js
+++ js/controllers.js
@@ -81,11 +81,37 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('soporteYAyudaCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('soporteYAyudaCtrl', ['$scope', '$stateParams', '$cordovaEmailComposer', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams,$cordovaEmailComposer) {
 
+$scope.clkenviar=function(_sel,_comen)
+{
+	console.log(localStorage.getItem('sel'));
+	localStorage.setItem('sel', _sel);
+	localStorage.setItem('comen', _comen);
+
+	document.addEventListener("deviceready", function () {
+
+		 $cordovaEmailComposer.isAvailable().then(function() {
+		   // is available
+		 }, function () {
+		   // not available
+		 });
+
+		  var email = {
+		    to: 'gerencia@aeromobilesoft.com.co',
+		    subject: localStorage.getItem('sel'),
+		    body: localStorage.getItem('comen'),
+		    isHtml: true
+		  };
+
+		 $cordovaEmailComposer.open(email).then(null, function () {
+		   // user cancelled email
+		 });
+	}, false);	
+}
 
 }])
    
@@ -105,3 +131,10 @@ function ($scope, $stateParams) {
 
 }])
  
+ .controller('comunidadCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
